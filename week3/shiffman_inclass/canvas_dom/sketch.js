01f991e9df3e882868b0320fe23a6e34fba6efc9
Daniel Shiffman <daniel@shiffman.net>
2014-04-11 15:47:59 -0400

    in class example cleanup

--- week3/shiffman_inclass/canvas_dom/sketch.js
+++ week3/shiffman_inclass/canvas_dom/sketch.js
@@ -1,33 +1,44 @@
 
+// Two canvases!
 var canvas1;
 var canvas2;
 
+// Location of one canvas
 var x = 0;
+var speed = 5;
 
+// Location of circle on canvas
 var ballx = 0;
 
 function setup() {  
-  canvas1 = createCanvas(600,400);
-  canvas2 = createCanvas(600,400);
-  background(0);
+  // Make both canvases
+  canvas1 = createCanvas(300,200);
+  canvas2 = createCanvas(300,200);
 }
 
 function draw() { 
+  // Canvas one is on one side
   canvas1.position(x,100);
-  canvas2.position(displayWidth-x,100);
+  // Canvas two is on the other side
+  canvas2.position(displayWidth-x-300,100);
 
+  // logic
+  x = x + speed;
+  ballx = ballx + speed;
+
+  if (ballx > width || ballx < 0) {
+    speed *= -1;
+  }
+
+  // Now deal with drawing on canvas 1
   context(canvas1);
-  background(0);
-  
+  background(0);  
   fill(255);
-  ellipse(ballx,200,40,40);
+  ellipse(ballx,100,40,40);
 
-  x++;
-  ballx++;
-
+  // No wdeal with drawing on canvas 2
   context(canvas2);
-  //background(255,0,0);
-
+  background(255,0,0);
 }
 
 function keyPressed() {
--- week3/shiffman_inclass/html_blah/sketch.js
+++
@@ -1,9 +0,0 @@
-
-function setup() {  
-  createCanvas(600, 400);
-  background(0);
-}
-
-function draw() { 
-
-}
