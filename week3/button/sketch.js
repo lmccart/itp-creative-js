9fca32412ae1b0b3e75141728af3f18df01d6b8f
Daniel Shiffman <daniel@shiffman.net>
2014-04-17 15:29:25 -0400

    added a slider to button example

--- week3/button/sketch.js
+++ week3/button/sketch.js
@@ -12,20 +12,35 @@ var counter;
 
 var button;
 
+var range;
+
+var rangeValue;
+
 function setup() {
   createCanvas(640,360);
   
   // I'm going to draw some text on top of the canvas!
   counter = createHTML("");
   counter.position(10,10);
-  counter.style("font-size: 16pt; color: #FFFFFF")
+  counter.style("font-size: 16pt; color: #FFFFFF");
   
   // A button
   button = createHTML("<button type=\"button\">Add a new object.</button>");
   button.position(10,40);
   // Attach the button to a function!
   button.mousePressed(newBall);
-  
+
+
+  // A range
+  var temp = createHTML("<input type=\"range\" id=\"size\" name=\"size\" min=\"10\" max=\"100\">");
+  temp.position(10,60);
+  range = document.getElementById("size");
+
+
+  rangeValue = createHTML("");
+  rangeValue.position(150,60);
+  rangeValue.style("color: #FFFFFF");
+
   // Initialize ball index 0
   balls.push(new Ball(50,0,24));
 
@@ -45,11 +60,18 @@ function draw() {
   // Updating the text!
   counter.html("Total objects: " + balls.length);
 
+
+  fill(255);
+  rangeValue.html("Size: " + range.value);
+
+
 }
 
 function newBall() {
+  //println(rang.value);
+
   // A new ball object
-  var b = new Ball(random(width),random(height),24); // Make a new object at the mouse location.
+  var b = new Ball(random(width),random(height),Number(range.value)); // Make a new object at the mouse location.
   // Here, the function push() adds an element to the end of the array. 
   balls.push(b);
 }
