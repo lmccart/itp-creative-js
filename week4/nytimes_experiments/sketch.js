cd7fb66378a99286933c8c0c5654c51706eb87e5
Daniel Shiffman <daniel@shiffman.net>
2014-04-18 19:44:37 -0400

    comments and fixes

--- week4/nytimes_experiments/sketch.js
+++ week4/nytimes_experiments/sketch.js
@@ -1,11 +1,13 @@
-// http://developer.nytimes.com
+// Count term appearance in times per year
+// Creative JS
+// Daniel Shiffman
 
-var term = 'terrorist';
-
+// An array of "Year" objects
 var years = [];
 
 
-function makeURL(year) {
+// Make a url that searhces for term appeared in a given year
+function makeURL(term,year) {
   var apikey = '&api-key=sample-key';
   var api = 'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?callback=svc_search_v2_articlesearch&';
   var query = 'q='+term+'&facet_field=source&begin_date='+year+'0101&end_date='+year+'1231&facet_filter=true';
@@ -15,46 +17,61 @@ function makeURL(year) {
 
 function setup() {
   createCanvas(800, 400);
-  for (var year = 2000; year < 2014; year++) {
-    years.push(new Year(year));
+  // Make year objects
+  for (var year = 1950; year < 2014; year++) {
+    years.push(new Year(year,'computer'));
   }
 
-  var temp = 0;
+  // We will count through the array each time this function is called
+  var count = 0;
   var grab = function() {
-    years[temp].loadData();
-    temp++;
+    years[count].loadData('computer');
+    count++;
   };
-  
+
   for (var i = 0; i < years.length; i++) {
-    setTimeout(grab,i*500);
+    // A little delay between each call so that we don't overload the API
+    setTimeout(grab,i*100);
   }
 }
 
 
 function draw() {
   background(51);
+  // Width of each bar
   var w = width/years.length;
   for (var i = 0; i < years.length; i++) {
     fill(255);
     stroke(0);
+    // Draw each bar
+    // Dividing by 50 is arbitrary
     rect(i*w,0,w,years[i].total/50);
     noFill();
     stroke(255);
     textAlign(CENTER);
-    text(years[i].year,i*w+w/2,height/2);
+    // Draw some years
+    if (i % 5 === 0) {
+      text(years[i].year,i*w+w/2,height/2);
+    }
   }
 
 }
 
-function Year(y) {
+// A new year object
+function Year(y,t) {
   this.year = y;
+  this.term = t;
   this.total = 0;
 }
 
+//a function to load the value
 Year.prototype.loadData = function() {
+  // Have to keep track of the context
   var self = this;
+  // This will be the callback, store the value returned in total
   var loaded = function(data) {
     self.total = data.response.facets.source.terms[0].count;
   };
-  loadJSON(makeURL(this.year), loaded);
+  // Load the data!
+  loadJSON(makeURL(this.term,this.year), loaded);
 };
