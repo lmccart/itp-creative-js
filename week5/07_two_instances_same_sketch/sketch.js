2610e4182ce1a065119daad2d6912dee4e1944b7
Daniel Shiffman <daniel@shiffman.net>
2014-04-23 12:43:08 -0400

    adding some animation to emphasize same sketch

--- week5/07_two_instances_same_sketch/sketch.js
+++ week5/07_two_instances_same_sketch/sketch.js
@@ -2,13 +2,20 @@ var s = function( sketch ) {
 
   var gray = 0; 
 
+  var x = 0;
+
   sketch.setup = function() {
     sketch.createCanvas(400, 400);
   };
 
   sketch.draw = function() {
     sketch.background(gray);
-    sketch.rect(sketch.width/2, sketch.height/2, 50, 50);
+    sketch.rect(x, sketch.height/2, 50, 50);
+
+    x = x + 5;
+    if (x > width) {
+      x = 0;
+    }
   }
 
   sketch.mousePressed = function() {
