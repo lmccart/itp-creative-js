c4b89100c9b495cd5fb8b687c93a89d091ca4c2d
lauren mccarthy <laurmccarthy@gmail.com>
2014-10-21 17:38:28 -0400

    proc convert ex

--- week1/processing_p5_conversion_1/sketch.js
+++ week1/processing_p5_conversion_1/sketch.js
@@ -1,24 +1,37 @@
 /**
- * Processing > Ex > Basics > Form > Bezier
- *
- * Bezier. 
+ * Processing > Ex > Basics > Color > RadialGradient
  * 
- * The first two parameters for the bezier() function specify the 
- * first point in the curve and the last two parameters specify 
- * the last point. The middle parameters set the control points
- * that define the shape of the curve.
- *
+ * Radial Gradient. 
+ * 
+ * Draws are series of concentric circles to create a gradient 
+ * from one color to another.
  */
 
-function setup() {           // **change** void setup() to var setup = function()
-  createCanvas(640, 360);  	 // **change** size() to createCanvas()
-  stroke(255);               // stroke() is the same
-  noFill();                  // noFill() is the same
+var dim;                                // change int to var
+
+function setup() {                      // change void to function
+  createCanvas(640, 360);               // change size to createCanvas
+  dim = width/2;
+  background(0);
+  colorMode(HSB, 360, 100, 100);
+  noStroke();
+  ellipseMode(RADIUS);
+  frameRate(1);
 }
 
-function draw() {                         // **change** void draw() to var setup = draw()
-  background(0);                          // background() is the same
-  for (var i = 0; i < 200; i += 20) {     // **change** int i to var i
-    bezier(mouseX-(i/2.0), 40+i, 410, 20, 440, 300, 240-(i/16.0), 300+(i/8.0)); // bezier() is the same
+function draw() {                       // change void to function
+  background(0);
+  for (var x = 0; x <= width; x+=dim) { // change int to var
+    drawGradient(x, height/2);
+  } 
+}
+
+function drawGradient(x, y) {           // change void to function, remove datatypes
+  var radius = dim/2;                   // change int to var
+  var h = random(0, 360);               // change float to var
+  for (var r = radius; r > 0; --r) {    // change int to var
+    fill(h, 90, 90);
+    ellipse(x, y, r, r);
+    h = (h + 1) % 360;
   }
 }
