965005d6196db7907efc88eb1f6b97f3d8d3f043
Lauren McCarthy <laurmccarthy@gmail.com>
2014-11-17 18:45:52 -0500

    minor

--- week5/01_p5_examples/02_loadJSON/sketch.js
+++ week5/01_p5_examples/02_loadJSON/sketch.js
@@ -15,18 +15,18 @@ function draw() {
 
 function drawData(data) {
 
-    background(120, 180, 200);
+  background(120, 180, 200);
 
-    // person 1 bubble
-    fill(155, 30, 180, 180);
-    ellipse(250, 200, data.person1.age * 5, data.person1.age * 5); // person1.age = 30
-    fill(255);
-    text(data.person1.name, 210, 200); // person1.name = Morgan
+  // person 1 bubble
+  fill(155, 30, 180, 180);
+  ellipse(250, 200, data.person1.age * 5, data.person1.age * 5); // person1.age = 30
+  fill(255);
+  text(data.person1.name, 210, 200); // person1.name = Morgan
 
-    // person 2 bubble
-    fill(180, 180, 34, 180);
-    ellipse(350, 200, data.person2.age * 5, data.person2.age * 5); // person2.age = 32
-    fill(255);
-    text(data.person2.name, 330, 200); // person2.name = Joss
+  // person 2 bubble
+  fill(180, 180, 34, 180);
+  ellipse(350, 200, data.person2.age * 5, data.person2.age * 5); // person2.age = 32
+  fill(255);
+  text(data.person2.name, 330, 200); // person2.name = Joss
 
 }
